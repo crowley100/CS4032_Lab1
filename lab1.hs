7a8e504d0054c61668aa340fa5b0edfd5df6fec6
Lorcan <loreidy@tcd.ie>
2016-11-02 20:13:39 +0000

    added buffering

--- lab1.hs
+++ lab1.hs
@@ -2,6 +2,7 @@ import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString
 import qualified Data.ByteString.Char8 as B8
 import System.IO
+import System.Posix.Unistd
 
 client :: String -> Int -> IO ()
 client host port = withSocketsDo $ do
@@ -10,14 +11,16 @@ client host port = withSocketsDo $ do
                 sock <- socket (addrFamily serverAddr) Stream defaultProtocol
                 connect sock (addrAddress serverAddr)
                 msgSender sock
-                sClose sock
 
 msgSender :: Socket -> IO ()
 msgSender sock = do
-  let msg = B8.pack "GET /server.php?message=its+high+noon HTTP/1.0\r\n\r\n"
-  send sock msg
-  rMsg <- recv sock 4096
-  B8.putStrLn rMsg
+  handle <- socketToHandle sock ReadWriteMode
+  hSetBuffering handle LineBuffering
+  hPutStrLn handle "GET /server.php?message=its+high+noon HTTP/1.0\r\n\r\n"
+  
+  rMsg <- hGetContents handle
+  putStrLn rMsg
+  hClose handle
   
 main :: IO ()
 main = client "localhost" 8000
